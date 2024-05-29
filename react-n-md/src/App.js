0d37bbf43a7fe5477fc967b49884babe98b0cbd4
Akanksha A <akankshaa@advantageclub.co>
2024-05-29 16:59:35 +0530

    first commit on my own

---
+++ react-n-md/src/App.js
@@ -0,0 +1,10 @@
+import './index.css'
+function App() {
+  return (
+    <div className="App ">
+      <div className='p-4'></div>
+    </div>
+  );
+}
+                              
+export default App;
